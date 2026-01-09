96a326e430e3140e1f96ff67b0ba5253b0712ceb
yugandhar-ds <yugandhar.dhandusubramanyam@riverty.com>
2026-01-09 12:10:45 +0100

    Fix React Router basename to match GitHub Pages path

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/hd">
+    <BrowserRouter basename="/h-d_boutique">
       <App />
       <Toaster 
         position="top-right"
